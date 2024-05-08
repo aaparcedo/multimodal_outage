3f028dd10571e51c33e8b0f8d08852f57b0aba9a
Christian Lopez <christianlopez@christianlopezmac.local>
2024-05-08 14:44:36 -0400

    modified our model architecture to take in a 5D vector of [batch_size, n_counties, n_channels, image_height, image_width].

--- graph_unet/Graph_Unet.py
+++ graph_unet/Graph_Unet.py
@@ -7,22 +7,29 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import numpy as np
 
-class DoubleConv(nn.Module):
+# Hyperparameters
 
+image_dimension = 128
+batch_size = 4
+n_counties = 67
+feature_vector_size = 8
+
+# Model Classes
+
+class DoubleConv(nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
         self.double_conv = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, bias=False),
-            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, bias=False), 
+            nn.ReLU(inplace=True),                                                     
             nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=False),
-            nn.ReLU(inplace=True)
+            nn.ReLU(inplace=True)                                                       
         )
 
     def forward(self, x):
         return self.double_conv(x)
     
 class Down(nn.Module):
-
     def __init__(self, in_channels, out_channels):
         super().__init__()
         self.maxpool_conv = nn.Sequential(
@@ -34,7 +41,6 @@ class Down(nn.Module):
         return self.maxpool_conv(x)
     
 class Up(nn.Module):
-
     def __init__(self, in_channels, out_channels):
         super().__init__()
 
@@ -59,7 +65,7 @@ class OutConv(nn.Module):
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
 
     def forward(self, x):
-        return self.conv(x)  
+        return self.conv(x)
     
 class Contraction(nn.Module):
     def __init__(self, in_channels): 
@@ -69,43 +75,38 @@ class Contraction(nn.Module):
         self.down2 = (Down(8, 16))
         self.down3 = (Down(16, 32))
         self.down4 = (Down(32, 64))
-        
-        self.feature_maps = []
-        
-    def feature_maps(self): 
-
-        return self.feature_maps
-    
-    def flattened_size(self): 
-        
-        return self.flattened_size
-    
-    def original_shape(self): 
-        
-        return self.original_shape
+        self.feature_maps = [[] for _ in range(4)]
     
     def forward(self, input):
+        encoder_input = []
         
-        x1 = self.inc(input)
-        self.feature_maps.append(x1)
+        for batch in range(batch_size):
         
-        x2 = self.down1(x1)
-        self.feature_maps.append(x2)
+            x1 = self.inc(input[batch])      
+            self.feature_maps[0].append(x1)            
+            
+            x2 = self.down1(x1)             
+            self.feature_maps[1].append(x2)
+            
+            x3 = self.down2(x2)            
+            self.feature_maps[2].append(x3)
 
-        x3 = self.down2(x2)
-        self.feature_maps.append(x3)
+            x4 = self.down3(x3)            
+            self.feature_maps[3].append(x4)
 
-        x4 = self.down3(x3)
-        self.feature_maps.append(x4)
-
-        x5 = self.down4(x4)
-        batch, _, _, _ = x5.shape
-        encoder_input = x5.view(batch, -1)
-    
+            x5 = self.down4(x4)             
+            encoder_input.append(x5)
+            
+        for feature_map in range(len(self.feature_maps)): 
+            self.feature_maps[feature_map] = torch.stack(self.feature_maps[feature_map])
+              
+        encoder_input = torch.stack(encoder_input)
+        encoder_input = encoder_input.view(batch_size, n_counties, -1)
+        
         return encoder_input
-
+    
 class Encoder(nn.Module):
-    def __init__(self, image_dimension):
+    def __init__(self):
         super(Encoder, self).__init__()
         self.downsized_image_dimension = image_dimension / 16
         self.first_layer_size = int(self.downsized_image_dimension * self.downsized_image_dimension * 64)
@@ -117,36 +118,47 @@ class Encoder(nn.Module):
         self.fc6 = nn.Linear(32, 16)
         self.fc7 = nn.Linear(16, 8)
 
-    def forward(self, x):
-        x = torch.relu(self.fc1(x))
-        x = torch.relu(self.fc2(x))
-        x = torch.relu(self.fc3(x))
-        x = torch.relu(self.fc4(x))
-        x = torch.relu(self.fc5(x))
-        x = torch.relu(self.fc6(x))
-        x = self.fc7(x)
-        return x
+    def forward(self, input):
+        wave_net_input = []
+        
+        for batch in range(batch_size):
+            x = torch.relu(self.fc1(input[batch]))
+            x = torch.relu(self.fc2(x))
+            x = torch.relu(self.fc3(x))
+            x = torch.relu(self.fc4(x))
+            x = torch.relu(self.fc5(x))
+            x = torch.relu(self.fc6(x))
+            x = self.fc7(x)
+            wave_net_input.append(x)
+            
+        wave_net_input = torch.stack(wave_net_input)
+        return wave_net_input
     
 class Decoder(nn.Module):
-    def __init__(self, input_size, batch_size, image_dimension):
+    def __init__(self):
         super(Decoder, self).__init__()
-        self.batch_size = batch_size
         self.downsized_image_dimension = int(image_dimension / 16)
         self.output_layer_size = int(self.downsized_image_dimension * self.downsized_image_dimension * 64)
-        self.fc1 = nn.Linear(input_size, 64)
+        self.fc1 = nn.Linear(feature_vector_size, 64)
         self.fc2 = nn.Linear(64, 128)
         self.fc3 = nn.Linear(128, 256)
         self.fc4 = nn.Linear(256, 512)
         self.fc5 = nn.Linear(512, self.output_layer_size) 
         
-    def forward(self, x):
-        x = torch.relu(self.fc1(x))
-        x = torch.relu(self.fc2(x))
-        x = torch.relu(self.fc3(x))
-        x = torch.relu(self.fc4(x))
-        x = self.fc5(x)
-        x = x.view(self.batch_size, 64, self.downsized_image_dimension, self.downsized_image_dimension)
-        return x
+    def forward(self, input):
+        expansion_input = []
+        
+        for batch in range(batch_size):
+            x = torch.relu(self.fc1(input[batch]))
+            x = torch.relu(self.fc2(x))
+            x = torch.relu(self.fc3(x))
+            x = torch.relu(self.fc4(x))
+            x = self.fc5(x)
+            expansion_input.append(x)
+            
+        expansion_input = torch.stack(expansion_input)
+        expansion_input = expansion_input.view(batch_size, n_counties, 64, self.downsized_image_dimension, self.downsized_image_dimension)
+        return expansion_input
     
 class Expansion(nn.Module):
     def __init__(self, output_channels): 
@@ -157,26 +169,35 @@ class Expansion(nn.Module):
         self.up4 = (Up(8, 4))
         self.outc = (OutConv(4, output_channels))
         
-    def forward(self, input, feature_maps): 
-        x = self.up1(input, feature_maps[-1])
-        x = self.up2(x, feature_maps[-2])
-        x = self.up3(x, feature_maps[-3])
-        x = self.up4(x, feature_maps[-4])
-        logits = self.outc(x)
-        return logits
+    def forward(self, input, feature_maps):
+        predictions = []
+        feature_map_iteration = 0
+        
+        for batch in range(batch_size):
+            x = self.up1(input[batch], feature_maps[-1][feature_map_iteration])
+            x = self.up2(x, feature_maps[-2][feature_map_iteration])
+            x = self.up3(x, feature_maps[-3][feature_map_iteration])
+            x = self.up4(x, feature_maps[-4][feature_map_iteration])
+            logits = self.outc(x)
+            predictions.append(logits)
+            feature_map_iteration += 1
+            
+        predictions = torch.stack(predictions)
+            
+        return predictions
     
 class Modified_UNET(nn.Module): 
-    def __init__(self, batch_size=67, input_channels=3, output_channels=3, feature_vector_size=8, image_dimension=128):
+    def __init__(self,input_channels=3, output_channels=3):
         super(Modified_UNET, self).__init__()
         self.contraction = Contraction(input_channels)
-        self.encoder = Encoder(image_dimension)
-        self.decoder = Decoder(feature_vector_size, batch_size, image_dimension)
+        self.encoder = Encoder()
+        self.decoder = Decoder()
         self.expansion = Expansion(output_channels)
         
     def forward(self, input): 
-        output = self.contraction(input)
+        output = self.contraction(input)        
         output = self.encoder(output)
-        output = self.decoder(output)        
+        output = self.decoder(output)
         feature_maps = self.contraction.feature_maps
         predicted_results = self.expansion(output, feature_maps)
         return predicted_results
