e6a19e86ea71acb567e61ad4967bb92a53a00455
aaparcedo <aaparcedo.io@gmail.com>
2024-05-07 22:05:55 -0400

    BlackMarbleDataset returns tensor

--- graph_unet/dataset_loader.py
+++ graph_unet/dataset_loader.py
@@ -9,18 +9,24 @@ class BlackMarbleDataset(Dataset):
     def __init__(self, data_dir, transform=None):
         self.data_dir = data_dir
         self.county_paths = os.listdir(data_dir)
-             
+        self.transform = transform if transform is not None else transforms.ToTensor() 
+    
     def __len__(self):
         return len(os.listdir(os.path.join(self.data_dir, 'orange')))
 
     def __getitem__(self, idx):
-      day_list = [] # hold image for {idx} for all 67 counties     
+      image_list = [] # hold image for {idx} for all 67 counties     
 
       for county in self.county_paths:
         county_path = os.path.join(self.data_dir, county)
         image_paths = os.listdir(county_path)
         image_path = os.path.join(county_path, image_paths[idx])  
         image = Image.open(image_path).convert('RGB')
-        day_list.append(image)
-
-      return day_list
+      
+        if self.transform:
+          image = self.transform(image)
+  
+        image_list.append(image)
+      
+      images = torch.stack(image_list)
+      return images
