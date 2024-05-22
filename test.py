c4eefbb091de5cc7418d824ab6d7ec631ac1bc16
aaparcedo <aaparcedo.io@gmail.com>
2024-05-21 21:56:59 -0400

    test file

---
+++ test.py
@@ -0,0 +1,95 @@
+import argparse
+import torch
+from torch.utils.data import DataLoader
+from torchvision import transforms
+import os
+import torch.nn as nn
+from utils import BlackMarbleDataset, mse_per_pixel, rmse_per_pixel, mae_per_pixel, mape_per_pixel, load_adj, load_checkpoint
+from models.unet  import Modified_UNET
+
+
+dir_image = "/groups/mli/multimodal_outage/data/black_marble/hq/percent_normal/"
+
+def test_model(epochs, batch_size, horizon, size, job_id, device):
+
+  randomadj = True
+  adjdata = "/home/aaparcedo/multimodal_outage/data/graph/adj_mx_fl_k1.csv"
+  adjtype = "doubletransition"
+
+  sensor_ids, sensor_id_to_ind, adj_mx = load_adj(adjdata,adjtype)  
+  supports = [torch.tensor(i).to(device) for i in adj_mx]
+
+  if randomadj:
+    adjinit = None
+  else:
+    adjinit = supports[0]
+
+  # should the model be the checkpoint
+  #model = Modified_UNET(supports).to(device=device)
+  model = load_checkpoint() # TODO: verify this works
+
+  transform = transforms.Compose([
+    transforms.ToTensor(),          # Convert to tensor
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+  ])
+
+  # Load dataset
+  dataset = BlackMarbleDataset(dir_image, size=size, start_index=horizon, transform=transform, evaluation=True)
+
+  print(f'size of dataset: {len(dataset)}')
+
+  # Create data loaders
+  loader_args = dict(batch_size=batch_size, num_workers=2)
+  test_loader = DataLoader(dataset, shuffle=True, **loader_args)
+
+  # Set up optimizer and custom loss function
+  criterion = mse_per_pixel
+  
+  # Alternative Benchmarks
+  rmse = rmse_per_pixel
+  mae = mae_per_pixel
+  mape = mape_per_pixel
+
+  model.eval()
+  test_loss = 0
+  test_rmse = 0
+  test_mae = 0
+  test_mape = 0
+  with torch.no_grad():
+    for item in test_loader:
+      past_tensor, future_tensor = (tensor.to(device).permute(0, 2, 1, 3, 4, 5) for tensor in item)
+      preds_tensor = model(past_tensor)
+      loss = criterion(preds_tensor, future_tensor)
+      test_rmse_loss = rmse(preds_tensor, future_tensor)
+      test_mae_loss = mae(preds_tensor, future_tensor)
+      test_mape_loss = mape(preds_tensor, future_tensor)
+      
+      test_loss += loss.item()
+      test_rmse += val_rmse_loss.item()
+      test_mae += val_mae_loss.item()
+      test_mape += val_mape_loss.item()
+
+  avg_test_loss = test_loss / len(test_loader)
+  avg_test_rmse_loss = test_rmse / len(test_loader)
+  avg_test_mae_loss = test_mae / len(test_loader)
+  avg_test_mape_loss = test_mape / len(test_loader)
+  
+  print(f'Test Results; Loss (MSE): {avg_test_loss:.4f}, RMSE: {avg_test_rmse_loss:.4f}, MAPE: {avg_test_mape_loss:.4f}, MAE: {avg_test_mae_loss:.4f}')
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batch_size', dest='batch_size', type=int, default=16, help='Batch size')
+    parser.add_argument('--horizon', dest='horizon', type=int, default=7, help='Timestep horizon')
+    parser.add_argument('--size', dest='size', type=str, default='S', help='Dataset size/horizon')
+    parser.add_argument('--job_id', dest='job_id', type=str, default='test', help='Slurm job ID')
+    parser.add_argument('--device', dest='device', type=str, default='cuda', help='Select device, i.e., "cpu" or "cuda"')
+    parser.add_argument('--checkpoint_path', dest='chck_path', type=str, help='Model checkpoint path to test')
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+  args = get_args()
+  device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+  test_model(epochs=args.epochs, batch_size=args.batch_size, horizon=args.horizon, size=args.size, job_id=args.job_id, device=args.device)
+
