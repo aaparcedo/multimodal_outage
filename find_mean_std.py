b3f93190ea4b50ed9b7b7f90c060033eafc8b49b
aaparcedo <aaparcedo.io@gmail.com>
2024-06-11 13:13:20 -0400

    find norm for ntl gray

--- find_mean_std.py
+++ find_mean_std.py
@@ -6,7 +6,8 @@ import pandas as pd
 ntl_dir = "/groups/mli/multimodal_outage/data/black_marble/hq/ntl"
 pon_dir = "/groups/mli/multimodal_outage/data/black_marble/hq/percent_normal/"
 pickle_dir = "/groups/mli/multimodal_outage/data/black_marble/hq/original"
-dir_image = ntl_dir
+ntl_gray_dir = "/groups/mli/multimodal_outage/data/black_marble/hq/ntl_gray"
+dir_image = ntl_gray_dir
 
 
 train_ia_id, test_m = {'h_ian': pd.Timestamp('2022-09-26'), 'h_idalia': pd.Timestamp('2023-08-30')}, {'h_michael': pd.Timestamp('2018-10-10')}
@@ -16,7 +17,7 @@ train_ia_m, test_id = {'h_ian': pd.Timestamp('2022-09-26'), 'h_michael': pd.Time
 entire_set = {'h_michael': pd.Timestamp('2018-10-10'), 'h_idalia': pd.Timestamp('2023-08-30'), 'h_ian': pd.Timestamp('2022-09-26')}
 
 
-dataset = BlackMarbleDataset(dir_image, size='M', case_study=entire_set)
+dataset = BlackMarbleDataset(dir_image, size='S', case_study=entire_set)
 data_loader = DataLoader(dataset, batch_size=1, shuffle=False, num_workers=2)
 
 mean = 0.0
@@ -26,16 +27,19 @@ n_samples = 0
 # future_tensor shape: torch.Size([1, 7, 67, 3, 128, 128])
 
 for data in data_loader:
-    past_tensor, future_tensor = data
 
-    # [1, 7, 67, 3, 128, 128]
+
+
+    past_tensor, future_tensor, _ = data
+
+    # [1, 7, 67, 1, 128, 128] 
     data_tensor = future_tensor  # Use future_tensor for normalization
 
-    # [67, 3, 128, 128]
-    data_tensor = data_tensor[0, 0, :, :, :, :]
+    # [67, 1, 128, 128]
+    data_tensor = data_tensor[0, 0, :, 0, :, :]
 
-    mean += data_tensor.mean(dim=(0, 2, 3), keepdim=True)
-    std += data_tensor.std(dim=(0, 2, 3), keepdim=True)
+    mean += data_tensor.mean(dim=(0, 1, 2))
+    std += data_tensor.std(dim=(0, 1, 2))
 
     n_samples += 1
 
